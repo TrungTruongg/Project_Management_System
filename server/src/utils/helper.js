94f0aaf0ebeb6db2bd11406b1281aec50e354644
TRUNG <trungtt1@tbsgroup.vn>
2026-03-03 13:40:53 +0700

    change port

--- server/src/utils/helper.js
+++ server/src/utils/helper.js
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
   service: process.env.EMAIL_SERVICE,
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
