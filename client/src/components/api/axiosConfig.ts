45243dd98239f6f61ba514ce422f64168407f03a
TRUNG <trungtt1@tbsgroup.vn>
2026-03-04 09:32:41 +0700

    change to railway

--- client/src/components/api/axiosConfig.ts
+++ client/src/components/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://project-management-system-rb5c.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || import.meta.env.VITE_SERVER_URL,
   headers: {
     'Content-Type': 'application/json',
   },
