a33cf611e76c2186249a5b2fcae48aa12a62806c
TRUNG <trungtt1@tbsgroup.vn>
2026-03-20 16:24:31 +0700

    fix

--- client/src/components/api/axiosConfig.ts
+++ client/src/components/api/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL || 'https://project-management-system-rb5c.onrender.com/api',
+    import.meta.env.VITE_API_URL || 'https://project-management-system-backend-cwpi.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
